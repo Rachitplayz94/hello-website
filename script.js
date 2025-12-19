8f85df1fc363cc0c140f4c6317dfdd93e1e3dc14
Rachitplayz94 <162964843+Rachitplayz94@users.noreply.github.com>
2025-12-20 03:23:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const commentList = document.getElementById("commentList");
+const commentInput = document.getElementById("commentInput");
+
+// Add Comment
+function addComment() {
+    const commentText = commentInput.value.trim();
+    if(commentText !== "") {
+        db.collection("comments").add({
+            text: commentText,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        commentInput.value = "";
+    } else {
+        alert("Please write a comment first!");
+    }
+}
+
+// Real-time listener
+db.collection("comments").orderBy("timestamp").onSnapshot((snapshot) => {
+    commentList.innerHTML = "";
+    snapshot.forEach(doc => {
+        const li = document.createElement("li");
+        li.textContent = doc.data().text;
+        commentList.appendChild(li);
+    });
+});
