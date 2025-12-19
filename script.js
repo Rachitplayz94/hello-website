8822a00716e02e75d18ef34cdc4ffcb814059555
Rachitplayz94 <162964843+Rachitplayz94@users.noreply.github.com>
2025-12-20 03:57:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const commentInput = document.getElementById("commentInput");
 // Add Comment
 function addComment() {
     const commentText = commentInput.value.trim();
+
     if(commentText !== "") {
         db.collection("comments").add({
+            name: window.userName || "Anonymous",
             text: commentText,
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
         });
@@ -20,8 +22,7 @@ db.collection("comments").orderBy("timestamp").onSnapshot((snapshot) => {
     commentList.innerHTML = "";
     snapshot.forEach(doc => {
         const li = document.createElement("li");
-        li.textContent = doc.data().text;
+        li.innerHTML = `<span class="comment-name">${doc.data().name}:</span> ${doc.data().text}`;
         commentList.appendChild(li);
     });
 });
-
